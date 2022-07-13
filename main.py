ae00c3d43f6055032a0f7b1e0aec1e04c0af540e
Niumandzi <amazheykin@gmail.com>
2022-07-13 18:13:26 +0300

    version 0.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -15,15 +16,23 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             for lm in hand_landmarks.landmark:
+            #Hands landmarks
                 height, width, channel = img.shape
                 cx, cy = int(lm.x * width), int(lm.y * height)
                 cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
-            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            print(
-                f'Index finger tip coordinates: (',
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width}, '
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height})'
-            )
+                mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            #Bounding box
+                x = [landmark.x for landmark in hand_landmarks.landmark]
+                y = [landmark.y for landmark in hand_landmarks.landmark]
+                center = np.array([np.mean(x) * width, np.mean(y) * height]).astype('int32')
+                cv2.circle(img, tuple(center), 10, (255, 0, 0), 1)  # for checking the center
+                cv2.rectangle(img, (center[0] - 200, center[1] - 200), (center[0] + 200, center[1] + 200), (255, 0, 0), 1)
+            #Finger coordinates
+                print(
+                    f'Index finger tip coordinates: (',
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width}, '
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height})'
+                )
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
